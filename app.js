9571e46daabc65af5a3119af219081a06ce57293
Basil Aslam <basilaslamnp@gmail.com>
2023-01-16 03:49:56 +0000

    fix some bugs

--- app.js
+++ app.js
@@ -6,7 +6,19 @@ require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
-let remainingTime = 0;
+
+
+const END_DATE = new Date("Monday, 20 February 2023 12:00:00")
+const START_DATE = new Date()
+const DIFF_TIME =  Math.abs(START_DATE - END_DATE)
+const DAYS_LEFT = Math.ceil(DIFF_TIME / (1000 * 60 * 60 * 24)); 
+const LEFT_DAYS_IN_S = DAYS_LEFT * 24 * 60 * 60 ;
+let remainingTime = LEFT_DAYS_IN_S;
+
+setInterval(()=>{
+    remainingTime = remainingTime-1;
+},1000)
+
 
 app.use((err, req, res, next) => {
     res.status(404).send("Sorry, that page doesn't exist!",err);
@@ -18,12 +30,6 @@ app.get('/api/remaining-time', (req, res) => {
   res.json({ remainingTime });
 });
 
-app.post('/api/remaining-time', express.json(), (req, res) => {
-
-  remainingTime = req.body.remainingTime;
-  res.json({ remainingTime });
-});
-
 app.listen(port, () => {
-  console.log('Server listening on port 3000');
+  console.log('Server listening on port '+port);
 });
